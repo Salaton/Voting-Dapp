1988fba45faed6527f68f542cdd02a23f4a0165b
Sala <elvisdenis056@gmail.com>
2018-11-11 12:50:48 +0300

    4. Watch Events

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,6 +16,11 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
+    //Add voted event
+    event votedEvent(
+        uint indexed _candidateId
+    );
+
     function Election () public {
         addCandidate("Elvis Salaton");
         addCandidate("Ian Minara");
@@ -39,5 +44,8 @@ contract Election {
 
         // update candidate vote Count
         candidates[_candidateId].voteCount ++;
+
+        //trigger voted event
+        votedEvent(_candidateId);
     }
 }
